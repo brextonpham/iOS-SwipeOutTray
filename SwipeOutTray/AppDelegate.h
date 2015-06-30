a20614764ef917136b31064a8b675f3c6d79cc17
Brexton Pham <brextonpham@stanford.edu>
2015-06-30 11:01:50 -0400

    Initial Commit

---
+++ SwipeOutTray/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SwipeOutTray
+//
+//  Created by Brexton Pham on 6/30/15.
+//  Copyright (c) 2015 Brexton Pham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
